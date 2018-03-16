61df1cea6e4fbccd2d611a3289a00e9f5f996cda
dchandra <chndraja@gmail.com>
2018-03-16 16:32:19 +0530

    changed to node-spamd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Spamc = require('spamc');
-var spamc = new Spamc();
-
+var Spamd = require("node-spamd");
+var spamd  = new Spamd('root', 'root', 'localhost', 783);
 
 
 /* GET home page. */
@@ -11,12 +10,35 @@ router
 .get('/', function(req, res, next) {
   res.render('index',{message:""});
 })
-.post('/',function(req,res,next){
-  spamc.report(req.body.message, function (result) {
-    console.log(result);
-    res.render('index',{message:result});
+.post('/',function(req,response,next){
 
+  spamd.evaluate(req.body.subject, req.body.message, function(res, err){
+    if(err) {
+     response.render('index',{
+       errMsg : "Error occured "+err
+     })
+    } else {
+      if(res.spam) {
+        var spamdResult = "The message is Spam, is evaluated with "+ res.evaluation + " points in a maximun of " + res.allowed;
+        response.render('index',{
+          message:spamdResult,
+          subject:req.body.subject,
+          messageBody:req.body.message,
+          spam:true,
+          rules:res.rules
+        });
+      }else{
+        var spamdResult = 'The message is not Spam, is evaluated with ' + res.evaluation + " points in a maximun of " + res.allowed;
+        response.render('index',{
+          message:spamdResult,
+          subject:req.body.subject,
+          messageBody:req.body.message,
+          spam:false
+        });
+      }
+    }
   });
+
 });
 
 module.exports = router;
